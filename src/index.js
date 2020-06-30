e827efedff5d7d97088207f5a429a98c60e6c444
Thiago Cedran <thiagocdn@gmail.com>
2020-06-29 21:29:49 -0300

    Configurado MySQL e Sequelizer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const db = require('./models');
+
+const authController = require('./constrollers/auth');
 
 const app = express();
+
+app.use('/auth', authController);
 
 app.get('/', (req, res) => {
   return res.json('Api running...');
 });
 
-app.listen(3001, () => {
-  console.log('Listening on port 3001');
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log('Listening on port 3001');
+  });
 });
 
