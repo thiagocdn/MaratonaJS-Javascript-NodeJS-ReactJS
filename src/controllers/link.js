b563451cf196c94e5f389cf6034fc3c91717321c
Thiago Cedran <thiagocdn@gmail.com>
2020-06-30 21:37:23 -0300

    Criado a rota createlink

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
   return res.jsonOK('Links');
 })
 
+router.post('/', async (req, res) => {
+  const { label, url, isSocial } = req.body;
+
+  
+
+  return res.jsonOK('Rota create link');
+})
+
 module.exports = router;
