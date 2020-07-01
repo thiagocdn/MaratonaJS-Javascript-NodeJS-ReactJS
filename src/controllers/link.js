71dfee250c12f1d60627d61e387bda1eddf5d762
Thiago Cedran <thiagocdn@gmail.com>
2020-06-30 21:51:57 -0300

    Implementada rota de edição de links

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Link } = require('../models');
 
 const router = express.Router();
 
@@ -7,11 +8,32 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
+  const accountId = 5;// req.id;
   const { label, url, isSocial } = req.body;
+  const image = 'https://google.com'
 
-  
+  const link = await Link.create({ label, url, isSocial, image, accountId })
 
-  return res.jsonOK('Rota create link');
-})
+  return res.jsonOK(link);
+});
+
+router.put('/:id', async (req, res) => {
+  const accountId = 5;// req.id;
+  const { id } = req.params;
+  const { body } = req;
+  const fields = ['label', 'url', 'isSocial'];
+
+  const link = await Link.findOne({ where: { id, accountId }});
+  if(!link) return res.jsonNotFound();
+
+  fields.map(fieldName => {
+    const newValue = body[fieldName];
+    if(newValue !== undefined) link[fieldName] = newValue;
+  });
+
+  await link.save();
+
+  return res.jsonOK(link);
+});
 
 module.exports = router;
