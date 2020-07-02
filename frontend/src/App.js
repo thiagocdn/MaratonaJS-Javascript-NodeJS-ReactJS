2b53da183d62dd57c63eec2b50478708e5e9ae9e
Thiago Cedran <thiagocdn@gmail.com>
2020-07-02 18:39:45 -0300

    Criado Layout paginas logado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './screens/Home';
 import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
 import Links from './screens/Manage/Links';
 import Create from './screens/Manage/Links/Create';
+import Edit from './screens/Manage/Links/Edit';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <nav>
-          <ul>
-            <li><Link to='/sign-in'>Sign in</Link></li>
-            <li><Link to='/sign-up'>Sign up</Link></li>
+          <ul className="list-group list-group-horizontal">
+            <li className="list-group-item"><Link to='/sign-in'>Sign in</Link></li>
+            <li className="list-group-item"><Link to='/sign-up'>Sign up</Link></li>
 
-            <li><Link to='/manage/links/create'>Create Link</Link></li>
-            <li><Link to='/manage/links/edit'>Edit Link</Link></li>
-            <li><Link to='/manage/links'>Links</Link></li>
+            <li className="list-group-item"><Link to='/manage/links/create'>Create Link</Link></li>
+            <li className="list-group-item"><Link to='/manage/links/edit'>Edit Link</Link></li>
+            <li className="list-group-item"><Link to='/manage/links'>Links</Link></li>
           </ul>
         </nav>
         <Switch>
@@ -24,9 +26,9 @@ const App = () => {
           <Route path='/sign-up'><SignUp /></Route>
 
           <Route path='/manage/links/create'><Create /></Route>
-          <Route path='/manage/links/edit'><h1>Edit Link</h1></Route>
+          <Route path='/manage/links/edit'><Edit /></Route>
           <Route path='/manage/links'><Links /></Route>
-          <Route path='/'><h1>Home</h1></Route>
+          <Route path='/'><Home /></Route>
         </Switch>
       </div>
     </BrowserRouter>
